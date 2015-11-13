fa8683b8ba11ba8de521abf1cfa46350117e4857
James Denman <james@fastmail.se>
2015-11-13 13:43:18 -0500

    first commit

---
+++ src/test/java/com/mkyong/pages/GoogleSearchResultsPage.java
@@ -0,0 +1,11 @@
+package com.mkyong.pages;
+
+import com.github.jsdevel.testng.selenium.AbstractPage;
+import java.net.URL;
+
+public class GoogleSearchResultsPage extends AbstractPage<GoogleSearchResultsPage, SamplePageFactory> {
+  @Override
+  public boolean isPageViewableFrom(URL proposedUrl) {
+    return "/search".equals(proposedUrl.getPath());
+  }
+}
